414db3adea0dba69816312bc6bf8baa4e94e9dc2
Marco Alvarez <okramuk@gmail.com>
2022-06-27 09:00:26 -0500

    Added ExampleContent, SCharacter & Setup Init
    Added Character class
    Added MoveForward/Turn input

---
+++ Source/AprendiendoUnreal/AprendiendoUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AprendiendoUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APRENDIENDOUNREAL_API AAprendiendoUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
